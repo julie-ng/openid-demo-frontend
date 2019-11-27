04ce1815cb1c75a0c0a49906310c1b87e1c161f2
Julie Ng <me@julie.io>
2019-11-27 13:57:00 +0100

    component: add demo navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
+    <AppHeader/>
     <router-view/>
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import AppHeader from '@/components/AppHeader.vue'
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "./scss/custom-bootstrap";
+:root {
+  --blue: #0078d7;
+  --green: #7FBA00;
+  --red: #F25022;
+  --orange: #FFB900;
+  --gray: #737373;
+  --primary: var(--blue);
+  --secondary: var(--green);
+  --body-weight: 400;
+  --body-color: #333;
+  --body-font: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  --header-height: 60px;
 }
 
-#nav {
-  padding: 30px;
+td {
+  word-break: break-word;
+  overflow-wrap: break-word;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  &:first-child {
+    min-width: 200px;
   }
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,78 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+    <div class="container">
+      <a class="navbar-brand" href="/">Azure AD</a> <span class="navbar-div">|</span>
+      <a class="navbar-title" href="/">OpenID Connect</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/profile">Profile</a>
+            <!-- <router-link class="nav-link" to="/">Home</router-link> -->
+          </li>
+          <li class="nav-item pl-3">
+            <a class="nav-link btn btn-outline-light" role="button" href="/logout">Logout</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  props: {
+    loggedIn: Boolean
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.navbar {
+  padding: 1em 0;
+}
+
+.navbar-brand {
+  font-weight: 600;
+  margin-right: 0;
+}
+
+.navbar-div {
+  font-weight: 400;
+  display: inline-block;
+  margin: 0 0.75em;
+  color: #fff;
+}
+
+.navbar-title {
+  font-weight: 400;
+  &:link,
+  &:visited {
+    color: rgba(255, 255, 255, 0.9);
+  }
+
+  &:hover,
+  &:active {
+    text-decoration: none;
+  }
+}
+
+.navbar-dark .navbar-nav {
+  .btn-outline-light {
+    &:link, :visted {
+      color: #fff;
+    }
+    &:hover, &:active {
+      color: var(--primary);
+    }
+  }
+}
+</style>
