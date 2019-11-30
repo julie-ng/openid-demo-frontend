1ab7a4a31e881467c6f46c05d3d031604200a781
Julie Ng <me@julie.io>
2019-11-30 12:02:11 +0100

    oidc: proxy redirects too

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const proxy = require('fastify-http-proxy')
 const port = process.env.PORT || 3000
 const apiHost = process.env.API_HOST
 
+// fastify.register(require('fastify-sensible'))
 fastify.register(assets, {
   root: path.join(__dirname, 'dist'),
   prefix: '/'
@@ -12,7 +13,28 @@ fastify.register(assets, {
 
 fastify.register(proxy, {
   upstream: apiHost,
-  prefix: '/api'
+  prefix: '/api/',
+  replyOptions: {
+    onResponse: (req, reply, res) => {
+      // handle redirects
+      console.log(res.headers)
+      if ([301, 302, 303, 307].includes(res.statusCode)) {
+        let i = res.rawHeaders.indexOf('Location')
+        if (i > -1) {
+          let location = res.rawHeaders[i + 1]
+          reply.redirect(location)
+        }
+      } else {
+        reply.send(res)
+      }
+      // fastify.log.warn('/Api Code? ', res.statusCode)
+      // console.log(res.rawHeaders)
+      // (res.statusCode < 400)
+      // 	? reply.send(res)
+      // 	: reply.send(fastify.httpErrors.badGateway())
+      // reply.send(res)
+    }
+  }
 })
 
 // Let VueJS handle 404s
